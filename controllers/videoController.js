4b13848ab4021a2ca75c122419c934df41a0f8f6
dongmikim <dongmi.public@gmail.com>
2020-06-23 00:51:42 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,12 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const search = (req, res) => res.render('search');
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render('search', { pageTitle: 'Search', searchingBy})
+};
+
 export const videos = (req, res) => res.render('videos');
 export const videoDetail = (req, res) => res.render('videoDetail');
 export const upload = (req, res) => res.render('upload');
