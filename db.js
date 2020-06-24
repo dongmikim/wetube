880aae8c646a8e836093d6302bad7febbf8ac1a3
dongmikim <dongmi.public@gmail.com>
2020-06-25 01:27:33 +0900

    Home controller, add fake db

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 324903,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Nicolas',
+      email: 'nico@las.com'
+    }
+  },
+  {
+    id: 234356,
+    title: 'Video amazing',
+    description: 'This is something I love',
+    views: 12,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Nicolas',
+      email: 'nico@las.com'
+    }
+  },
+  {
+    id: 565768,
+    title: 'Video cooooool',
+    description: 'This is something I love',
+    views: 14,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Nicolas',
+      email: 'nico@las.com'
+    }
+  }
+]
