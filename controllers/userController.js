8406a783e7ec350357cc8a7f9151c63daaee07ba
dongmi kim <dongmi.public@gmail.com>
2020-06-26 19:32:03 +0900

    join controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,12 @@
-export const join = (req, res) => res.render('join');
+export const getJoin = (req, res) => {
+  res.render('join', { pageTitle : 'Join' });
+}
+
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  res.render('join', { pageTitle : 'Join' });
+}
+
 export const login = (req, res) => res.render('login');
 export const logout = (req, res) => res.render('logout');
 export const users = (req, res) => res.render('users');
