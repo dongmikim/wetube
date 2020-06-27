e5c862a2132e29412aaf6b0d1c23f26c8fc2511f
dongmikim <dongmi.public@gmail.com>
2020-06-28 01:53:21 +0900

    Add more controlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,36 @@
+import routes from '../routes';
+
 export const getJoin = (req, res) => {
   res.render('join', { pageTitle : 'Join' });
 }
 
 export const postJoin = (req, res) => {
-  console.log(req.body);
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if(password !== password2) {
+    res.status(400);
+  } else {
+    res.redirect(routes.home)
+  }
+  // To Do: Register user
+  // To Do: Log user in
   res.render('join', { pageTitle : 'Join' });
 }
 
-export const login = (req, res) => res.render('login');
-export const logout = (req, res) => res.render('logout');
+export const getLogin = (req, res) => {
+  res.render('login');
+} 
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+}
+
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+
+}
 export const users = (req, res) => res.render('users');
 export const userDetail = (req, res) => res.render('userDetail');
 export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'Edit Profile'});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from '../db';
+import routes from '../routes';
 
 export const home = (req, res) => {
   res.render('home', { pageTitle: 'Home', videos });
@@ -14,6 +15,19 @@ export const search = (req, res) => {
 };
 
 export const videoDetail = (req, res) => res.render('videoDetail');
-export const upload = (req, res) => res.render('upload');
+
+export const getUpload = (req, res) => {
+  res.render('upload', { pageTitle: 'Upload' });
+};
+
+
+export const postUpload = (req, res) => {
+  const {
+    body: { videoFile, title, description }
+  } = req;
+  // To Do: Upload and save videos
+  res.redirect(routes.videoDetail(324903));
+};
+
 export const editVideo = (req, res) => res.render('editVideo');
 export const deleteVideo = (req, res) => res.render('delete Video');
