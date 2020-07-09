e807a8aa41902ad36b5eb0c95fc8be355526b440
dongmi kim <dongmi.public@gmail.com>
2020-07-09 20:49:05 +0900

    Upload video with async and await

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,14 @@
 import routes from '../routes';
+import Video from '../models/Video';
 
-export const home = (req, res) => {
-  res.render('home', { pageTitle: 'Home', videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render('home', { pageTitle: 'Home', videos });
+  } catch (error) {
+    console.log(error);
+    res.render('home', { pageTitle: 'Home', videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -22,12 +29,18 @@ export const getUpload = (req, res) => {
 };
 
 
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
   const {
-    body: { videoFile, title, description }
+    body: { title, description },
+    file:  { path }
   } = req;
-  // To Do: Upload and save videos
-  res.redirect(routes.videoDetail(324903));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const editVideo = (req, res) => res.render('editVideo');
